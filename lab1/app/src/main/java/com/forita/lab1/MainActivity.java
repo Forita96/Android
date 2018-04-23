3bb6a30d2d30760c4de9e265bec0f96b530d3ad8
MARGARET\Рита <forita28@gmail.com>
2018-04-23 16:24:53 +0200

    Лаба3

--- lab1/app/src/main/java/com/forita/lab1/MainActivity.java
+++ lab1/app/src/main/java/com/forita/lab1/MainActivity.java
@@ -1,14 +1,28 @@
 package com.forita.lab1;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.Manifest;
+import android.widget.Toast;
+
+//import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 Button button1;
 Button button2;
+     Thread thread;
+    private int PERMISSION_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +42,49 @@ Button button2;
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
-                startActivity(intent);
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this,"You've already granted this permission",Toast.LENGTH_SHORT).show();
+                }else {
+                    requestPermission();
+                }
             }
         });
 
+
     }
 
+    private void requestPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_CONTACTS)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission")
+                    .setMessage("WE just need it")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                          ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.READ_CONTACTS},PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
 
+        }else {
+           ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_CONTACTS}, PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == PERMISSION_CODE){
+            if(grantResults.length>0  && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this,"Permission Denied",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
--- lab1/app/src/main/java/com/forita/lab1/SecondActivity.java
+++ lab1/app/src/main/java/com/forita/lab1/SecondActivity.java
@@ -3,27 +3,60 @@ package com.forita.lab1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends Activity {
 Button button1_secondAct;
+Thread thread;
+int mCounter=0;
+    final Handler myHandler = new Handler(); // автоматически привязывается к текущему потоку.
+
 //CalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_second);
-
+        final TextView txtResult = (TextView)findViewById(R.id.textView1);
         button1_secondAct = (Button) findViewById(R.id.button);
 
         button1_secondAct.setOnClickListener(new View.OnClickListener() {
+            final String result = doLongAndComplicatedTask();
             @Override
+
             public void onClick(View v) {
-                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
-                startActivity(intent);
+            thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10000);
+                    doLongAndComplicatedTask();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+});
+thread.start();
+                txtResult.setText(result);
             }
         });
+    }
+
+    private String doLongAndComplicatedTask() {
+        long res=0;
+        for(int i=0;i<100000;i++)
+        {
+            res=res+i;
+
+        }
+        return "Результат = "+res;
     }
 }
